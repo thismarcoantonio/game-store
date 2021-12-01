95c9c999b6bc9b95f908e523703816d6d613e45e
thismarcoantonio <thismarcoantonio@gmail.com>
2021-12-01 19:23:34 -0300

    Update Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,8 +11,8 @@ export function Header({ children }) {
     setOpen((state) => !state);
   };
 
-  const getActiveProps = (location) => {
-    return { className: "bg-green-400" };
+  const getActiveProps = (tone) => () => {
+    return { className: `bg-green-${tone}` };
   };
 
   return (
@@ -21,19 +21,23 @@ export function Header({ children }) {
         <RiGamepadLine size={24} className="mr-2" />
         <span className="font-semibold text-xl tracking-tight">Game store</span>
       </Link>
-      <div className="flex text-white lg:order-1">
-        <Link to="/cart" className="mr-4 lg:mr-0">
+      <div className="flex text-white items-center lg:order-1">
+        <Link
+          to="/cart"
+          getActiveProps={getActiveProps(600)}
+          className="flex items-center justify-center w-8 h-8 rounded-full mr-4 lg:mr-0 hover:bg-green-600"
+        >
           <RiShoppingCartLine size={20} />
         </Link>
         <RiMenu3Line size={20} onClick={toggleMenu} className="cursor-pointer select-none lg:hidden" />
       </div>
       <div className={clsx({ hidden: !open }, "w-full block flex-grow lg:flex lg:items-center lg:w-auto lg:block", )}>
         <div className="text-sm lg:flex-grow">
-          {routes.map((route) => (
+          {routes.filter((route) => !route.meta.hidden).map((route) => (
             <Link
               to={route.path}
               key={route.path}
-              getActiveProps={getActiveProps}
+              getActiveProps={getActiveProps(400)}
               className="block mt-4 px-3 py-2 rounded-sm text-white lg:inline-block lg:mt-0 lg:mr-2 lg:py-1"
             >
               {route.meta.title}
